bb45c091be563c373aeae1194e551a7d21ff28c1
Bill Sun <billyuanhong.sun@mail.utoronto.ca>
2019-01-26 12:01:42 -0500

    1

--- test.py
+++ test.py
@@ -9,6 +9,8 @@ tiers = np.array([[10, -10, 10,  500,  500,  9],
                   [20, -20, 30, 1000, 1000, 15],
                   [25, -25, 40, 1250, 1250, 18],
                   [30, -30, 50, 1500, 1500, 21]])
+tier = 4
+
 
 tracks = []
 max_speed = []
@@ -18,7 +20,7 @@ for i in range(8):
     max_vel = []
     for j in range(1000):
         if track[j+1] >= 0:
-            max_vel.append(math.sqrt(track[j+1]*tiers[2, 5]/1000000))
+            max_vel.append(math.sqrt(track[j+1]*tiers[tier, 5]/1000000))
         else:
             max_vel.append(-1)
     max_speed.append(np.array(max_vel))

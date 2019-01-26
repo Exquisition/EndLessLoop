8617d1440d3cdf352440baadf37aa3c1e031f9e5
Bill Sun <billyuanhong.sun@mail.utoronto.ca>
2019-01-26 11:57:07 -0500

    test1

--- test.py
+++ test.py
@@ -3,7 +3,12 @@ from numpy import genfromtxt
 import math
 
 
-handling_coeff = 15
+# Acceleration, Breaking, Top Speed, Gas Capacity, Tire Durability, Handling
+tiers = np.array([[10, -10, 10,  500,  500,  9],
+                  [15, -15, 20,  750,  750, 12],
+                  [20, -20, 30, 1000, 1000, 15],
+                  [25, -25, 40, 1250, 1250, 18],
+                  [30, -30, 50, 1500, 1500, 21]])
 
 tracks = []
 max_speed = []
@@ -13,7 +18,7 @@ for i in range(8):
     max_vel = []
     for j in range(1000):
         if track[j+1] >= 0:
-            max_vel.append(math.sqrt(track[j+1]*handling_coeff/1000000))
+            max_vel.append(math.sqrt(track[j+1]*tiers[2, 5]/1000000))
         else:
             max_vel.append(-1)
     max_speed.append(np.array(max_vel))
